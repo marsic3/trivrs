5450253aa61cc703e6757fac91f135dd57e4bfe0
marsic3 <milos.arsic@lotusflare.com>
2021-11-20 21:36:00 +0100

    triv

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="h-16 flex py-4 px-4 w-full mb-6 md:mb-0">
     <p class="mx-auto">
-      <a href="/" class="text-purple-500">Diligaf</a> {{ date }}
+      <a href="/" class="text-purple-500">Triv</a> {{ date }}
     </p>
   </div>
 </template>
